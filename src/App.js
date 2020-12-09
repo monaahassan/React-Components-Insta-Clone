8ceee6ac234a1c8e84ec2c8645ea8934e18d686d
Mona Hassan <monamia@Monas-MacBook-Pro.local>
2020-12-08 20:38:08 -0600

    React Insta Clone

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState} from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import Posts from "./Posts/Posts.js";
+import SearchBar from "./components/SearchBar.js"
 // Import the dummyData
+import DummyData from "./dummy-data.js"
 import './App.css';
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
+
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
 
